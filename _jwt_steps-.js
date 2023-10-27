0f5320b40a2074cd4a72cb6dfb53a58dbe4404c0
Sushil97 <sushil500530@gmail.com>
2023-10-27 18:15:55 +0600

    set jwt and uses load data in client side

---
+++ _jwt_steps-.js
@@ -0,0 +1,23 @@
+/***
+ * 
+ * 1. install jsonqwebtoken
+ * 
+ */
+
+/****
+ *  how to store token in the client side
+ * 
+ * 1. memory(js variable ,context) ----> ok tyoe
+ * 2. local storage ----> ok type (xss)
+ * 3. cookies : http only
+ * 
+ */
+
+/**
+ * 1. set cookies with http only(ati keno kora hoy--> google it) for development secure: false(production er jonno true dibo)
+ * 2. cors setting 
+ * 
+ * 3. client side axios setting
+ * 
+ * 
+ */
